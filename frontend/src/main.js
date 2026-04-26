09f3a91d077005a3dbfc8e988f1791d716c87b6d
Leo Sibaja <albert.sibaja@ucr.ac.cr>
2026-04-25 23:22:37 -0600

    initial files to have the login

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory} from "vue-router";
+import LandingPage from "./components/LandingPage.vue";
+import LoginForm from './components/LoginForm.vue';
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path : "/", name: "Landing", component: LandingPage },
+        { path : "/login", name: "Login", component: LoginForm },
+    ],
+});
+
+createApp(App).use(router).mount('#app')
