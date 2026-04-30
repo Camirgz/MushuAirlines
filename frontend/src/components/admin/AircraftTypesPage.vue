978814e8df6b6c739852c8f1af853ed70e243989
AlexanderEduc <alexander.wangwu@ucr.ac.cr>
2026-04-30 03:51:03 -0600

    Adding template files for testing

---
+++ frontend/src/components/admin/AircraftTypesPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page">
+    <h1>Lista de aeronaves</h1>
+    <p>Aquí se administrarán los aeronaves de Mushu Airlines.</p>
+
+    <RouterLink to="/admin" class="back-btn">
+      Volver al panel
+    </RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AircraftTypesPage",
+};
+</script>
+
+<style scoped>
+.page {
+  padding: 40px;
+}
+
+.back-btn {
+  color: #f01818;
+  font-weight: 700;
+}
+</style>
---
+++ frontend/src/components/admin/RoutesPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page">
+    <h1>Lista de rutas</h1>
+    <p>Aquí se administrarán las rutas de Mushu Airlines.</p>
+
+    <RouterLink to="/admin" class="back-btn">
+      Volver al panel
+    </RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RoutesPage",
+};
+</script>
+
+<style scoped>
+.page {
+  padding: 40px;
+}
+
+.back-btn {
+  color: #f01818;
+  font-weight: 700;
+}
+</style>
