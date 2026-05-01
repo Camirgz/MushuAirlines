5e5a3bf44c545171e046fdf4321901e66b841419
Leo Sibaja <albert.sibaja@ucr.ac.cr>
2026-05-01 17:21:41 -0600

    adding routes and logical restrictions to the main.js

--- frontend/src/components/LandingPageInter.vue
+++ frontend/src/components/LandingPageInter.vue
@@ -2,6 +2,9 @@
   <div class="container mt-5">
     <h1 class="display-4 text-center">Landing Page Interno</h1>
   </div>
+        <a href="/create-profile" class="btn btn-outline-danger rounded-pill px-3 py-1 admin-btn">
+            <i class="bi bi-person me-1"></i>create profile
+        </a>
 </template>
 
 <script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,9 @@ import {createRouter, createWebHistory} from "vue-router";
 import LandingPage from "./components/LandingPage.vue";
 import LoginForm from './components/LoginForm.vue';
 import LandingPageInter from './components/LandingPageInter.vue';
+import AdminCreateEmployee from './components/CreateProfile.vue';
+import CompleteRegister from './components/CompleteRegister.vue';
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -11,18 +14,35 @@ const router = createRouter({
         { path : "/", name: "Landing", component: LandingPage },
         { path : "/login", name: "Login", component: LoginForm },
         { path : "/admin", name: "Admin", component: LandingPageInter, meta: { requiresAuth: true }  },
+        { path : "/create-profile", name: "createProfile", component: AdminCreateEmployee, meta: { requiresAuth: true, requiresAdmin: true} },
+        { path : "/complete-register", name: "CompleteRegister", component: CompleteRegister }
     ],
 });
+
+function getRoleFromToken() {
+    const token = localStorage.getItem("token");
+    if (!token) return null;
+
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    return payload["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+}
 
 router.beforeEach((to, from, next) => {
     // we check if the route requires authentication and if we have a token in localStorage
     const token = localStorage.getItem("token");
-
     if (to.meta.requiresAuth && !token) {
-        next("/login");
-    } else {
-        next();
+        return next("/login");
+    } 
+    if (to.meta.requiresAdmin) {
+        const role = getRoleFromToken();
+
+        if (role !== "Administrator") {
+            // to the intern landing page, only admins
+            return next("/admin"); 
+        }
     }
+
+    next();
 });
 
 createApp(App).use(router).mount('#app')
