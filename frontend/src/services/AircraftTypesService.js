9cde65267b8e987a986273db58a7865c66230bfa
Daniel Rojas <24danielrm@gmail.com>
2026-05-04 00:36:28 -0600

    Create service layer for aircraft types operations

---
+++ frontend/src/services/AircraftTypesService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const BaseUrl = "http://localhost:5103/api/aircraft-types";
+
+export function GetAircraftTypes() {
+  return axios.get(BaseUrl);
+}
+
+export function CreateAircraftType(Payload) {
+  return axios.post(BaseUrl, Payload);
+}
+
