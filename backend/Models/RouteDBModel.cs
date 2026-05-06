a820d94594e547dae7b5272054bda3dadf39e0b9
Arianna Cortes <ariannacortesr@gmail.com>
2026-05-05 22:24:08 -0600

    Added extra information such as amount of seats per class and start/end date of the route service for the external api.

--- backend/Models/RouteDBModel.cs
+++ backend/Models/RouteDBModel.cs
@@ -11,6 +11,8 @@ public class RouteDbModel
     public string ArrivalTime { get; set; }
     public string Duration { get; set; }
     public string AircraftTypeId { get; set; }
+    public string StartDate { get; set; }
+    public string FinalizationDate { get; set; }
     public string Frequency { get; set; }
     public decimal PriceFirstClass { get; set; }
     public decimal PriceEconomy { get; set; }
@@ -19,4 +21,6 @@ public class RouteDbModel
     public decimal BagPrice { get; set; }
     public decimal BagWeight { get; set; }
     public decimal BagMultiplier { get; set; }
+    public int EconomyClassCapacity { get; set; }
+    public int FirstClassCapacity { get; set; }
 }
