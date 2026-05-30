1f290686368072ea0b394f68041160cb3533f6a7
Daniel Rojas <24danielrm@gmail.com>
2026-05-29 22:51:18 -0600

    Configure purchase service dependencies

--- backend/Program.cs
+++ backend/Program.cs
@@ -24,6 +24,11 @@ builder.Services.AddScoped<backend.Interfaces.IPassengerRepository,  backend.Rep
 builder.Services.AddScoped<backend.Interfaces.IItineraryRepository,  backend.Repositories.ItineraryRepository>();
 builder.Services.AddScoped<backend.Interfaces.IPurchaseRepository,   backend.Repositories.PurchaseRepository>();
 
+builder.Services.AddSingleton<backend.Interfaces.ICodeGenerator,     backend.Services.CodeGenerator>();
+builder.Services.AddSingleton<backend.Interfaces.IPricingCalculator, backend.Services.PurchasePricingCalculator>();
+builder.Services.AddScoped<backend.Services.RouteCreationService>();
+builder.Services.AddScoped<backend.Interfaces.IPurchaseService,      backend.Services.PurchaseService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
