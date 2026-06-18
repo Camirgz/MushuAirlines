5908283ff16849d1a6d11a54d277ba6551856cdb
Camirgz <camila.rodriguez.aguila@gmail.com>
2026-06-17 19:55:31 -0600

    CORS try

--- backend/Program.cs
+++ backend/Program.cs
@@ -13,17 +13,16 @@ var jwtKey = "MushuClaveLeo.ari,Cami,alex;dani";
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                    policy =>
-                    {
-                        policy.WithOrigins(
-                            "http://localhost:8080",
-                            "https://mushu-airlines.vercel.app",
-                            "https://mushu-airlines-git-dev-camirgzs-projects.vercel.app"
-                        )
-                            .AllowAnyHeader()
-                            .AllowAnyMethod();
-                    });
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:8080",
+            "https://mushu-airlines.vercel.app",
+            "https://mushu-airlines-git-dev-camirgzs-projects.vercel.app"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
 });
 
 builder.Services
@@ -31,54 +30,20 @@ builder.Services
     .AddJwtBearer(options =>
     {
         options.RequireHttpsMetadata = false;
-
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = false,
             ValidateAudience = false,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
-
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(jwtKey)
-            ),
-
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
             ClockSkew = TimeSpan.Zero
         };
     });
 
 builder.Services.AddAuthorization();
 
-
-builder.Services.AddScoped<
-    IPurchaseConfirmationRepository,
-    PurchaseConfirmationRepository>();
-
-builder.Services.AddScoped<
-    IQrService, 
-    QrService>();
-
-builder.Services.AddScoped<
-    IPaymentRepository,
-    PaymentRepository>();
-    
-builder.Services.AddScoped<
-    IEmailPurchaseService,
-    EmailPurchaseService>();
-    
-builder.Services.AddScoped<
-    IPaymentService,
-    PaymentService>();
-
-builder.Services.AddScoped<
-    PurchaseConfirmationService>();
-
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
-        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
-    });
+// Services (sin duplicados)
 builder.Services.AddScoped<IPurchaseConfirmationRepository, PurchaseConfirmationRepository>();
 builder.Services.AddScoped<IQrService, QrService>();
 builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
@@ -86,18 +51,11 @@ builder.Services.AddScoped<IEmailPurchaseService, EmailPurchaseService>();
 builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddScoped<PurchaseConfirmationService>();
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-        options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase);
-
 builder.Services.AddScoped<IExternalApiRepository, ExternalApiRepository>();
 builder.Services.AddScoped<ExternalApiService>();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-
-builder.Services.AddScoped<backend.Interfaces.IPassengerRepository,  backend.Repositories.PassengerRepository>();
-builder.Services.AddScoped<backend.Interfaces.IPurchaseRepository,   backend.Repositories.PurchaseRepository>();
+builder.Services.AddScoped<backend.Interfaces.IPassengerRepository, backend.Repositories.PassengerRepository>();
+builder.Services.AddScoped<backend.Interfaces.IPurchaseRepository, backend.Repositories.PurchaseRepository>();
 
 builder.Services.AddScoped<IAirportRepository, AirportRepository>();
 builder.Services.AddScoped<IAirportService, AirportService>();
@@ -117,10 +75,21 @@ builder.Services.AddScoped<IProfileService, ProfileService>();
 builder.Services.AddScoped<IFlightRepository, RouteCreationRepository>();
 builder.Services.AddScoped<FlightAggregatorService>();
 
-builder.Services.AddSingleton<backend.Interfaces.ICodeGenerator,     backend.Services.CodeGenerator>();
+builder.Services.AddSingleton<backend.Interfaces.ICodeGenerator, backend.Services.CodeGenerator>();
 builder.Services.AddSingleton<backend.Interfaces.IPricingCalculator, backend.Services.PurchasePricingCalculator>();
 builder.Services.AddScoped<backend.Interfaces.IRouteCreationService, backend.Services.RouteCreationService>();
-builder.Services.AddScoped<backend.Interfaces.IPurchaseService,      backend.Services.PurchaseService>();
+builder.Services.AddScoped<backend.Interfaces.IPurchaseService, backend.Services.PurchaseService>();
+
+// AddControllers una sola vez con ambas opciones
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -131,7 +100,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors(MyAllowSpecificOrigins);
-//app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
