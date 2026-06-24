e5a14d7a2ed596cc3583a9840b9938cc5ac44cdf
AlexanderEduc <alexander.wangwu@ucr.ac.cr>
2026-06-23 19:44:22 -0600

    Fix errors

--- backend/Models/AirportCatalogModel.cs
+++ backend/Models/AirportCatalogModel.cs
@@ -2,6 +2,6 @@ namespace backend.Model;
 
 public class AirportCatalogModel
 {
-    public string Country { get; set; }
-    public string City { get; set; }
+    public string Country { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
 }
